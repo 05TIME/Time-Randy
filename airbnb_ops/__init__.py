490f1b947fa3e9031c9d9fde6109bb934fc25b11
Randy Idaeho <timeoso@outlook.com>
2026-08-23 17:47:24 +0100

    Export Airbnb finance and agent services

--- airbnb_ops/__init__.py
+++ airbnb_ops/__init__.py
@@ -4,8 +4,11 @@ from .service import AirbnbOpsService
 from .notifications import TurnoverStatus, TurnoverTask, create_turnover_task
 from .forecast import OccupancyForecast, forecast
 from .escalation import Escalation, EscalationLevel, assess
+from .finance import FinanceSnapshot, build_snapshot
+from .agent import OpsRecommendation, recommend
 
 __all__ = [
     "AirbnbOpsService", "TurnoverStatus", "TurnoverTask", "create_turnover_task",
     "OccupancyForecast", "forecast", "Escalation", "EscalationLevel", "assess",
+    "FinanceSnapshot", "build_snapshot", "OpsRecommendation", "recommend",
 ]
