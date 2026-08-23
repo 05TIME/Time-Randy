a0c0014afc86bf2777e7e26a8f05195ccce5cb53
Randy Idaeho <timeoso@outlook.com>
2026-08-23 17:53:06 +0100

    Export booking channel adapters

--- airbnb_ops/__init__.py
+++ airbnb_ops/__init__.py
@@ -6,9 +6,12 @@ from .forecast import OccupancyForecast, forecast
 from .escalation import Escalation, EscalationLevel, assess
 from .finance import FinanceSnapshot, build_snapshot
 from .agent import OpsRecommendation, recommend
+from .channels import BookingChannelAdapter, ExternalBooking, ManualChannelAdapter
+from .sync import sync_channel
 
 __all__ = [
     "AirbnbOpsService", "TurnoverStatus", "TurnoverTask", "create_turnover_task",
     "OccupancyForecast", "forecast", "Escalation", "EscalationLevel", "assess",
     "FinanceSnapshot", "build_snapshot", "OpsRecommendation", "recommend",
+    "BookingChannelAdapter", "ExternalBooking", "ManualChannelAdapter", "sync_channel",
 ]
