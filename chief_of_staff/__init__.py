81211a01846da7182cdcb54691c97610b3dc8ba3
Randy Idaeho <timeoso@outlook.com>
2026-08-23 18:24:17 +0100

    feat: add Chief of Staff v2 package

---
+++ chief_of_staff/__init__.py
@@ -0,0 +1,5 @@
+"""TIMEŒ Chief of Staff v2 orchestration layer."""
+
+from .orchestrator import BusinessUnitSignal, ChiefOfStaffDecision, rank_signals
+
+__all__ = ["BusinessUnitSignal", "ChiefOfStaffDecision", "rank_signals"]
